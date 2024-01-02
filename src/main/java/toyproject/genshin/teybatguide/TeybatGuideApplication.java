c3622c71831987814ef9ed397f1765e80ec2a4cc
ahma0 <dsd932@naver.com>
2024-01-03 03:13:33 +0900

    chore: project setting

---
+++ src/main/java/toyproject/genshin/teybatguide/TeybatGuideApplication.java
@@ -0,0 +1,13 @@
+package toyproject.genshin.teybatguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeybatGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeybatGuideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/toyproject/genshin/teybatguide/TeybatGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package toyproject.genshin.teybatguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeybatGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
