d2ad242af31eac71d7294b7b3858130406ded837
ahma0 <dsd932@naver.com>
2024-01-27 14:03:25 +0900

    feat: 캐릭터 리스트 api 구현

--- src/main/java/toyproject/genshin/teybatguide/controller/CharactersController.java
+++ src/main/java/toyproject/genshin/teybatguide/controller/CharactersController.java
@@ -2,10 +2,11 @@ package toyproject.genshin.teybatguide.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import toyproject.genshin.teybatguide.controller.dto.SaveCharacterRequest;
+import toyproject.genshin.teybatguide.controller.dto.CharacterListResponse;
 import toyproject.genshin.teybatguide.service.CharactersService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/characters")
@@ -13,5 +14,10 @@ import toyproject.genshin.teybatguide.service.CharactersService;
 public class CharactersController {
 
     private final CharactersService charactersService;
+
+    @GetMapping
+    public ResponseEntity<List<CharacterListResponse>> getCharacterList() {
+        return ResponseEntity.ok(charactersService.findAndCreateCharacterList());
+    }
 
 }
--- src/main/java/toyproject/genshin/teybatguide/service/CharactersService.java
+++ src/main/java/toyproject/genshin/teybatguide/service/CharactersService.java
@@ -2,11 +2,22 @@ package toyproject.genshin.teybatguide.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import toyproject.genshin.teybatguide.controller.dto.CharacterListResponse;
+import toyproject.genshin.teybatguide.domain.Characters;
 import toyproject.genshin.teybatguide.repository.CharactersRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CharactersService {
 
     private final CharactersRepository charactersRepository;
+
+    public List<CharacterListResponse> findAndCreateCharacterList() {
+        return charactersRepository.findAll()
+                .stream()
+                .map(CharacterListResponse::of)
+                .toList();
+    }
 }
