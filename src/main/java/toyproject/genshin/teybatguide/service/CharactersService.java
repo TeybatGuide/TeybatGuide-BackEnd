7703b70b59f09d3b61b88a7969075d773a1b3667
ahma0 <dsd932@naver.com>
2024-01-27 18:30:24 +0900

    feat: 이미지 전송 구현

--- src/main/java/toyproject/genshin/teybatguide/service/CharactersService.java
+++ src/main/java/toyproject/genshin/teybatguide/service/CharactersService.java
@@ -1,14 +1,20 @@
 package toyproject.genshin.teybatguide.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import toyproject.genshin.teybatguide.controller.dto.CharacterDetailsResponse;
 import toyproject.genshin.teybatguide.controller.dto.CharacterListResponse;
 import toyproject.genshin.teybatguide.domain.Characters;
+import toyproject.genshin.teybatguide.domain.Resources;
+import toyproject.genshin.teybatguide.exception.TeybatException;
 import toyproject.genshin.teybatguide.repository.CharactersRepository;
 
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class CharactersService {
 
@@ -19,5 +25,26 @@ public class CharactersService {
                 .stream()
                 .map(CharacterListResponse::of)
                 .toList();
+    }
+
+    public FileSystemResource findImage(String id) {
+
+        return openFile(id);
+
+    }
+
+    public FileSystemResource openFile(String id) {
+        try {
+            String path = "/Users/ahnnayeong/img/character/" + id + "_1.webp";
+            FileSystemResource resource = new FileSystemResource(path);
+
+            if (!resource.exists()) {
+                throw new TeybatException("이미지 파일 없음");
+            }
+
+            return resource;
+        } catch (Exception e) {
+            throw new TeybatException("이미지 없음");
+        }
     }
 }
