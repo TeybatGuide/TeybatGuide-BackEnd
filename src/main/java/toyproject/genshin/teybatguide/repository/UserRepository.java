0eb989f582ceab584680be049921f2b04c9afa36
ahma0 <dsd932@naver.com>
2024-02-27 16:53:55 +0900

    feat: UserRepository 생성

---
+++ src/main/java/toyproject/genshin/teybatguide/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package toyproject.genshin.teybatguide.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import toyproject.genshin.teybatguide.domain.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByLoginId(String loginId);
+
+}
