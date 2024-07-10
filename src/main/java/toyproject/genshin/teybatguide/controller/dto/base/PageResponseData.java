8a3e30b8f7817ed03e3913ddef4803e6376ada5d
ahma0 <dsd932@naver.com>
2024-07-10 23:51:39 +0900

    feat: pageResponseData 메시지 기능 확장

--- src/main/java/toyproject/genshin/teybatguide/controller/dto/base/PageResponseData.java
+++ src/main/java/toyproject/genshin/teybatguide/controller/dto/base/PageResponseData.java
@@ -2,14 +2,22 @@ package toyproject.genshin.teybatguide.controller.dto.base;
 
 import org.springframework.data.domain.Pageable;
 
-public record PageResponseData<T>(T wrapper, PageDto page) {
+public record PageResponseData<T>(T wrapper, String message, PageDto page) {
+
+    public static <T> PageResponseData<T> of(T wrapper, String message, PageDto page) {
+        return new PageResponseData<T>(wrapper, message, page);
+    }
 
     public static <T> PageResponseData<T> of(T wrapper, PageDto page) {
-        return new PageResponseData<T>(wrapper, page);
+        return new PageResponseData<T>(wrapper, null, page);
+    }
+
+    public static <T> PageResponseData<T> of(T wrapper, String message, Pageable pageable) {
+        return new PageResponseData<T>(wrapper, message, PageDto.of(pageable));
     }
 
     public static <T> PageResponseData<T> of(T wrapper, Pageable pageable) {
-        return new PageResponseData<T>(wrapper, PageDto.of(pageable));
+        return new PageResponseData<T>(wrapper, null, PageDto.of(pageable));
     }
 
 }
